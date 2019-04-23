75a8b0237f42a6c2dfd4381b27abf78491a6d2a7
Philip Martinelli <Looker@Philip-Martinellis-MacBook-Air.local>
2019-04-23 10:29:53 -0700

    committing try/catch block

--- src/actions/slack/slack.ts
+++ src/actions/slack/slack.ts
@@ -107,8 +107,12 @@ https://github.com/looker/actions/blob/master/src/actions/slack/README.md`,
       limit: 200,
     }
     async function pageLoaded(accumulatedChannels: any[], response: any): Promise<any[]> {
-      const mergedChannels = accumulatedChannels.concat(response.channels)
-
+      try {
+            const mergedChannels = accumulatedChannels.concat(response.channels);
+      }
+      catch(e) {
+        console.error(e);
+      }
       // When a `next_cursor` exists, recursively call this function to get the next page.
       if (response.response_metadata &&
           response.response_metadata.next_cursor &&
